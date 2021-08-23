15ef26be638ee2a90f068c6c10ecc7f9fbbfa44d
Gaurav <bhattgauravd@gmail.com>
2021-08-23 23:46:14 +0530

    Added Navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <button className="link-btn">Coming Soon</button>
+        <div className="logo">
+          <h2>MiniBeats</h2>
+        </div>
+        <button className="link-btn">Coming Soon</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
