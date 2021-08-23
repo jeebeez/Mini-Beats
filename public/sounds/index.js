fc036775dd23f86f4108dafbbdb57d4d1c10d611
Gaurav <bhattgauravd@gmail.com>
2021-08-24 00:08:09 +0530

    Added last changes and exported sounds

---
+++ public/sounds/index.js
@@ -0,0 +1,45 @@
+export { default as hihat808 } from "./hihat-808.wav";
+export { default as hihatacoustic01 } from "./hihat-acoustic01.wav";
+export { default as kick808 } from "./kick-808.wav";
+export { default as kickclassic } from "./kick-classic.wav";
+export { default as kickheavy } from "./kick-heavy.wav";
+export { default as kicksofty } from "./kick-softy.wav";
+export { default as snare808 } from "./snare-808.wav";
+export { default as snareacoustic01 } from "./snare-acoustic01.wav";
+export { default as snarevinyl02 } from "./snare-vinyl02.wav";
+
+// export * from "./sounds";
+// import {
+//   hihat808,
+//   hihatacoustic01,
+//   kick808,
+//   kickclassic,
+//   kickheavy,
+//   kicksofty,
+//   snare808,
+//   snareacoustic01,
+//   snarevinyl02,
+// } from "../assets/sounds/index.js";
+
+// const [kickAudio, setKickAudio] = useState(kickclassic);
+
+// const [snareAudio, setSnareAudio] = useState(snareacoustic01);
+
+// const [hihatAudio, setHihatAudio] = useState(hihatacoustic01);
+
+// // const kickAudioSound, snareAudioSound, hihatAudioSound;
+// let kickAudioSound = new Audio(`${kickAudio}`);
+// let snareAudioSound = new Audio(`${snareAudio}`);
+// let hihatAudioSound = new Audio(`${hihatAudio}`);
+
+// useEffect(() => {
+//   // console.log(`${kickAudio}`);
+//   // let kickAudioState = require(kickAudio);
+//   // let snareAudioState = require(snareAudio);
+//   // let hihatAudioState = require(hihatAudio);
+
+//   // React.lazy(() => import(kickAudio));
+//   // React.lazy(() => import(snareAudio));
+//   // React.lazy(() => import(hihatAudio));
+
+// }, [kickAudio, snareAudio, hihatAudio]);
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Player from "./components/Player";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
